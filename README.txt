70969fa43330717c25a2f00cf2700668776150f8
Ning Wang <email@ningwang.org>
2017-11-09 20:25:03 -0800

    add the missing packages

--- README.txt
+++ README.txt
@@ -1,7 +1,7 @@
-1. build the webapp
-   ./sbuild/build_all.sh
+1. install the webapp
+   ./sbuild/install.sh
 
 2. create a docker image for the backend
    cd cattleshed/docker
    ./get_bin.sh
-   docker image build
+   docker image build -t 
--- sbuild/install.sh
+++ sbuild/install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 mypath=$(dirname $(readlink -f $0))
 
-docker image build -t webapp_docker . 
+echo "create the build environment"
+docker image build -t webapp_docker ${mypath}
+
+echo "build the code in the build environment"
 docker run --rm -it -v ${mypath}/../../webapp:/root/webapp webapp_docker /root/webapp/sbuild/build_all.sh
+
+echo "build cattleshed execution environment"
+cd ${mypath}/../cattleshed/docker
+./get_bin.sh
+docker image build -t cattleshed .
