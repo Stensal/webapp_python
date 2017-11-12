3b713e2cef86e42bec6039531835b8c1a248224f
Ning Wang <email@ningwang.org>
2017-11-11 21:28:28 -0800

    use tmpfs

--- installation/install.sh
+++ installation/install.sh
@@ -5,7 +5,7 @@ echo "create the build environment"
 docker image build -t webapp_docker ${mypath}
 
 echo "build the code in the build environment"
-docker run --rm -it -v ${mypath}/../../webapp:/root/webapp webapp_docker /root/webapp/sbuild/build_all.sh
+docker run --rm -it -v ${mypath}/../../webapp:/root/webapp webapp_docker /root/webapp/installation/build_all.sh
 
 echo "prepare build cattleshed execution environment"
 cd ${mypath}/../cattleshed/docker
