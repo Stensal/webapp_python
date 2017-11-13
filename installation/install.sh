c1d7264d23dd4972eda6089e5bc464341e3c21a0
Ning Wang <email@ningwang.org>
2017-11-12 21:48:23 -0800

    build php5 integration

--- installation/install.sh
+++ installation/install.sh
@@ -7,6 +7,10 @@ docker image build -t webapp_docker ${mypath}
 echo "build the code in the build environment"
 docker run --rm -it -v ${mypath}/../../webapp:/root/webapp webapp_docker /root/webapp/installation/build_all.sh
 
+# build cppcms php integration
+cd ${mypath}/../cppcms/contrib/integration/session/php/php5
+env CPPCMS_PATH=${mypath}/../build/assembly_line ./build_ext.sh
+
 echo "prepare build cattleshed execution environment"
 cd ${mypath}/../cattleshed/docker
 ./get_bin.sh
